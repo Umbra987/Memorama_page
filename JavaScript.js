a5743238bbbb07ca772eb36b5b8febc3b4eb2ea9
Daniel Rico <107763710+Umbra987@users.noreply.github.com>
2023-01-11 13:33:39 -0500

    Add files via upload

--- JavaScript.js
+++ JavaScript.js
@@ -8,3 +8,34 @@ copiarlink.addEventListener('click',()=>{
 });
 
 //-----------------------------------------------------------------
+
+//  Juego de Memorama 
+
+let imagenes = [
+   "img/parejas-001.png",
+   "img/parejas-002.png",
+   "img/parejas-003.png",
+   "img/parejas-004.png",
+   "img/parejas-005.png",
+   "img/parejas-006.png",
+   "img/parejas-007.png",
+   "img/parejas-008.png",
+];
+
+//Generación aleatoria de las iamgenes en el tablero.
+var cadena_pos_imaganes =[1,1,2,2,3,3,4,4,5,5,6,6,7,7,8,8];
+var cadena = ['X','X','X','X','X','X','X','X','X','X','X','X','X','X','X','X'];
+function mezclador_tarjetas(a){ 
+      random = Math.floor(Math.random()*16);
+      if(a<16){
+         if(cadena_pos_imaganes[random]!='X'){
+         cadena[a]=cadena_pos_imaganes[random];
+         cadena_pos_imaganes[random]= 'X';
+         mezclador_tarjetas(a+1);
+         }
+         else mezclador_tarjetas(a);
+   
+
+         }
+}
+mezclador_tarjetas(0);
